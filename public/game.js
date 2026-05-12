c630c0b4e9c651475b5ead82b43323107eb72bf1
Ravel Zahfino <ravelzahfino1234@gmail.com>
2026-05-13 06:02:21 +0700

    PIPA GAP

--- public/game.js
+++ public/game.js
@@ -840,7 +840,7 @@ function drawPowerupHUD() {
 // ══════════════════════════════════════════
 
 function createPipe() {
-    let gap = 130;
+    let gap = 140;
     let minPipeHeight = 60;
     let pipeTopHeight = Math.random() * (canvas.height - gap - minPipeHeight * 2) + minPipeHeight;
     pipes.push({ x: canvas.width, y: 0, width: 52, height: pipeTopHeight, type: 'top', scored: false });
